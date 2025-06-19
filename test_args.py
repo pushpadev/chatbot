cd3a89f599678804295e57578a1db436fdee41a6
pushpadev <pushpa.fullstackdeveloper@gmail.com>
2025-06-19 23:53:02 +0530

    fix: rmeove unused files

--- test_args.py
+++
@@ -1,6 +0,0 @@
-import sys
-with open('args_output.txt', 'w') as f:
-    f.write('Arguments:\n')
-    for i, arg in enumerate(sys.argv[1:], 1):
-        f.write(f'Arg {i}: {arg}\n')
-    f.write('Done.\n') 
